d665800b825ae6162fc0a3b12fb0a710914d2846
Danilo Bossolani <danbossolani@gmail.com>
2026-07-10 00:55:47 -0300

    feat: adiciona conexao com PostgreSQL via pool

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
+import { pool } from './config/database';
 import express from 'express'
 const app = express()
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+app.listen(3000, async () => {
+    console.log('Server is running on port 3000');
+
+    try {
+        const result = await pool.query('SELECT NOW()');
+        console.log('Conectado ao banco! Hora atual:', result.rows[0]);
+    } catch (error) {
+        console.error('Erro ao conectar no banco:', error);
+    }
+});
